9ba503adeaf4f5231843d6c9457c45210b8cebc8
ertex47 <ertex47@gmail.com>
2021-03-29 19:43:13 +0000

    drop db

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -7,7 +7,7 @@ class Enrollment < ApplicationRecord
   validates_uniqueness_of :user_id, scope: :course_id #user cant be subscribed to the same course twice
   validates_uniqueness_of :course_id, scope: :user_id #user cant be subscribed to the same course twice
   
-  validate :cant_subscibe_to_own_course #user cant create subscription if course.user==current_user to the same course twice
+  validate :cant_subscribe_to_own_course #user cant create subscription if course.user==current_user to the same course twice
   
     
   def to_s
@@ -15,10 +15,10 @@ class Enrollment < ApplicationRecord
   end  
   
   protected
-  def cant_subscibe_to_own_course
+  def cant_subscribe_to_own_course
     if self.new_record?
-      if self.user_id.presence?
-        if user_id==course.user_id
+      if self.user_id.present?
+        if self.user_id==course.user_id
           errors.add(:base, "You can not subscribe to your own course")
         end
       end  
