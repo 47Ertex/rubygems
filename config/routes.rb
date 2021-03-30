7f2204ce70a1a1f1c4f3c88ec32640ef7b5dcb98
ertex47 <ertex47@gmail.com>
2021-03-30 13:12:30 +0000

    104 106 108

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :enrollments
   devise_for :users
   resources :courses do
-      resources :lessons
+    resources :enrollments, only: [:new, :create]
+    resources :lessons
   end    
   resources :users, only: [:index, :edit, :show, :update]
   get 'home/index'
