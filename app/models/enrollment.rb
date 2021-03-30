99ec7d5fdbd3067069434a3de54902bb812c5bf2
ertex47 <ertex47@gmail.com>
2021-03-30 14:41:48 +0000

    button for user to review a course

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -9,6 +9,7 @@ class Enrollment < ApplicationRecord
   
   validate :cant_subscribe_to_own_course #user cant create subscription if course.user==current_user to the same course twice
   
+  scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
     
   def to_s
     user.to_s + " " + course.to_s
