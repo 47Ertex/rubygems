2cbac4166a24e3e022ec2d7342c4a105f856e09c
ertex47 <ertex47@gmail.com>
2021-04-07 20:42:53 +0000

    138 140

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,14 +1,24 @@
 class Lesson < ApplicationRecord
   belongs_to :course, counter_cache: true
+  
+  has_many :user_lessons
   #Course.find_each { |course| Course.reset_counters(course.id, :lessons) }
   validates :title, :content, :course,  presence: true
   has_rich_text :content
-  def to_s
-    title
-  end
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
   include PublicActivity::Model
   tracked owner: Proc.new{|controller, model| controller.current_user}
+  
+    
+  def to_s
+    title
+  end
+  
+  def viewed(user)
+    self.user_lessons.where(user: user).present?
+    #self.user_lessons.where(user_id: [user.id], lesson_id: [self.id]).empty?
+  end  
 end
