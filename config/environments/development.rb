a113b1b0bda27711988e2a0857ed3366a95d7fff
ertex47 <ertex47@gmail.com>
2021-04-25 07:24:54 +0000

    confirm email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.hosts << "606036ea1c2041378cf94284f8bcc84a.vfs.cloud9.eu-north-1.amazonaws.com"
-  config.action_mailer.default_url_options = { host: 'https://606036ea1c2041378cf94284f8bcc84a.vfs.cloud9.eu-north-1.amazonaws.com'}
+  config.action_mailer.default_url_options = { host: 'https://ertex47-app.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
