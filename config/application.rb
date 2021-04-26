6d82d4565a17cae71a27e02971e96875d5f67782
ertex47 <ertex47@gmail.com>
2021-04-26 10:02:21 +0000

    205 207 209

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,26 @@ module Ruygems
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+  
     if Rails.env.development? #for rails-erd gem to generate a diagram
       def eager_load!
         Zeitwerk::Loader.eager_load_all
       end
+    end
+    
+  
+    
+    config.after_initialize do
+      #ActionText::ContentHelper.allowed_attributes << "style"
+      #ActionText::ContentHelper.allowed_attributes << "controls"
+      #ActionText::ContentHelper.allowed_tags << "audio"
+      #ActionText::ContentHelper.allowed_tags << "video"
+      #ActionText::ContentHelper.allowed_tags << "source"
+      ActionText::ContentHelper.allowed_attributes.add 'style'
+      ActionText::ContentHelper.allowed_attributes.add 'controls'
+      ActionText::ContentHelper.allowed_tags.add 'video'
+      ActionText::ContentHelper.allowed_tags.add 'audio'
+      ActionText::ContentHelper.allowed_tags.add 'source'
     end  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
